f5166103948552404e0b77a779eff781b9ca19c9
akimotoibuki <ikb2420006@stu.o-hara.ac.jp>
2025-11-13 11:20:13 +0900

    html

---
+++ ikisaki/template/home.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const toggleButton = document.getElementById("toggle-genre");
+    const extraGenres = document.getElementById("extra-genres");
+
+    toggleButton.addEventListener("click", () => {
+        extraGenres.classList.toggle("hidden");
+        toggleButton.textContent = extraGenres.classList.contains("hidden") ? "▼" : "▲";
+    });
+});
