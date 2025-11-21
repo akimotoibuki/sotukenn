50c67a72407d6778737c6f195f6461d1b7defa16
akimotoibuki <ikb2420006@stu.o-hara.ac.jp>
2025-11-21 12:24:12 +0900

    a

--- ikisaki/template/home.js
+++ ikisaki/template/home.js
@@ -1,13 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const toggleButton = document.getElementById("toggle-genre");
-    const extraGenres = document.getElementById("extra-genres");
-
-    toggleButton.addEventListener("click", () => {
-        extraGenres.classList.toggle("hidden");
-        toggleButton.textContent = extraGenres.classList.contains("hidden") ? "▶" : "◀";
-    });
-});
-document.addEventListener("DOMContentLoaded", () => {
     // ▼ ジャンル切り替え
     const toggleButton = document.getElementById("toggle-genre");
     const extraGenres = document.getElementById("extra-genres");
