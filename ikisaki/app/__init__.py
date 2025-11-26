05aee36efe8357077bc064d7ee26f6be8c30d7e6
akimotoibuki <ikb2420006@stu.o-hara.ac.jp>
2025-11-26 11:59:38 +0900

    w

--- ikisaki/app/__init__.py
+++ ikisaki/app/__init__.py
@@ -1,10 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
 
 def create_app():
-    app = Flask(__name__)
-    app.config.from_object("app.config.Config")
+    app = Flask(__name__, static_folder='static', template_folder='templates')
 
-    from app.routes.spot_routes import spot_bp
-    app.register_blueprint(spot_bp)
+    app.config.from_object("app.config.Config")  # ← config.py が必要
+
+    db.init_app(app)
+    Migrate(app, db)
+
+    # Blueprint登録
+    from .home import home_bp
+    from .login import login_bp
+    from .register import register_bp
+    app.register_blueprint(home_bp)
+    app.register_blueprint(login_bp)
+    app.register_blueprint(register_bp)
 
     return app
---
+++ ikisaki/app/config.py
@@ -0,0 +1,5 @@
+# app/config.py
+class Config:
+    SECRET_KEY = 'your-secret-key'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///users.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
