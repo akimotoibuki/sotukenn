bcc968cf266bac43c19bcf0b0a847c8f1996bdc9
akimotoibuki <ikb2420006@stu.o-hara.ac.jp>
2026-01-14 14:14:41 +0900

    d

--- ikisaki/app/__init__.py
+++ ikisaki/app/__init__.py
@@ -1,16 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from .config import Config
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__, static_folder='static', template_folder='templates')
-
-    app.config.from_object("app.config.Config")  # ← config.py が必要
+    app.config.from_object(Config)
 
     db.init_app(app)
-    Migrate(app, db)
+    migrate.init_app(app, db)
 
     # Blueprint登録
     from .home import home_bp
--- ikisaki/app/config.py
+++ ikisaki/app/config.py
@@ -1,5 +1,7 @@
-# app/config.py
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
-    SECRET_KEY = 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///users.db'
+    SECRET_KEY = 'your-very-secret-key'  # ← 任意の文字列でOK
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'instance', 'users.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
